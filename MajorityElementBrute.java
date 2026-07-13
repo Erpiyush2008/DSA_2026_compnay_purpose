a8e99f3e6563655d0e99cdffc907cf867b27c278
Erpiyush2008 <piyushsharma190708@gmail.com>
2026-07-13 09:40:19 +0530

    hello

--- MajorityElementBrute.java
+++ MajorityElementBrute.java
@@ -30,3 +30,35 @@ public class MajorityElementBrute {
         System.out.println(majorityElement(nums));
     }
 }
+
+/*
+public class MajorityElement {
+
+    public static int majorityElement(int[] nums) {
+
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (candidate == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2,2,1,1,1,2,2};
+
+        System.out.println("Majority Element = " + majorityElement(nums));
+    }
+} */
